97751bc69d0de83d236ce90d4191de0982ca129e
Mohamed Shosha <moshosha267@gmail.com>
2025-10-19 05:52:24 +0300

    "fix(cors): enable CORS for rasein.vercel.app and localhost"

--- src/main/java/com/trace/controller/VisionController.java
+++ src/main/java/com/trace/controller/VisionController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@CrossOrigin(origins = "*")
+@RequestMapping("/api/vision")
 public class VisionController {
 
     @Autowired
